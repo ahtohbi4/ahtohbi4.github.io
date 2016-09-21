9c1eb76acfb70e10199b50e0c533d34ef5d9e72d
Alexander Antonov <alexandr-post@yandex.ru>
2016-09-21 21:18:11 +0300

    data/ was created

---
+++ data/projects.js
@@ -0,0 +1,18 @@
+export const PROJECTS = {
+    0: {
+        name: 'Tic-tac-toe',
+        source: 'http://ya.ru',
+        description: '<p>Game</p>',
+        tech: [
+            20, 3, 4
+        ]
+    },
+    1: {
+        name: 'Poker',
+        source: 'http://ya.ru',
+        description: '<p>Game</p>',
+        tech: [
+            20, 3, 4
+        ]
+    }
+};
