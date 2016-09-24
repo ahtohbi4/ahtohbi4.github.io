eca5a98950e32ec86e7060242a60028b6a51fab6
Alexander Antonov <alexandr-post@yandex.ru>
2016-09-25 00:06:16 +0300

    data/person.yaml was created

--- app/resources/views/pages/index.jsx
+++ app/resources/views/pages/index.jsx
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
-const PROJECTS = require('../../../../data/projects.yaml').projects;
-const TECHS = require('../../../../data/technologies.yaml').technologies;
+const PERSON = require('../../../../data/person.yaml');
+const PROJECTS = require('../../../../data/projects.yaml');
+const TECH = require('../../../../data/tech.yaml');
 
 /**
  * @class
@@ -10,9 +11,36 @@ const TECHS = require('../../../../data/technologies.yaml').technologies;
  */
 class Root extends Component {
     render() {
+        const now = new Date();
+
+        const dateOfBirth = new Date(PERSON.dateOfBirth);
+
+        const age = now.getFullYear() - dateOfBirth.getFullYear();
+
         return (
             <div>
-                <h1>Projects</h1>
+                <h1>{PERSON.name}</h1>
+                <p>Age: {age}</p>
+                <p>Place: {PERSON.place}</p>
+
+                <h2>Experience</h2>
+                {PERSON.experience.map((job, i) => {
+                    return (
+                        <div key={i}>
+                            <h5>{job.position}</h5>
+                            {(() => {
+                                const dateFrom = new Date(job.dateFrom);
+                                const dateTo = new Date(job.dateTo);
+
+                                return (
+                                    <p>{dateFrom.getFullYear()}-{dateTo.getFullYear()}</p>
+                                );
+                            })()}
+                        </div>
+                    );
+                })}
+
+                <h2>Project</h2>
 
                 {Object.keys(PROJECTS).map((id) => {
                     const project = PROJECTS[id];
@@ -21,10 +49,12 @@ class Root extends Component {
                         <div key={id}>
                             <h3>{project.name}</h3>
 
+                            <div dangerouslySetInnerHTML={{__html: project.description}}/>
+
                             <ul>
                                 {project.tech.map((techID) => {
                                     return (
-                                        <li key={techID}>{TECHS[techID].name}</li>
+                                        <li key={techID}>{TECH[techID].name}</li>
                                     );
                                 })}
                             </ul>
