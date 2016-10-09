4c0d013e81dd7d2fd00ea20f424973193250cf2a
Alexander Antonov <alexandr-post@yandex.ru>
2016-10-09 12:29:25 +0300

    / page was updated

--- app/resources/views/pages/main.jsx
+++ app/resources/views/pages/main.jsx
@@ -1,13 +1,34 @@
 import React, {Component} from 'react';
 
+import Immutable from 'immutable';
+
+import PERSON from '../../../../data/person.yaml';
+
 /**
  * @class
  * @extends Component
  */
 export default class Main extends Component {
     render() {
+        const person = Immutable.fromJS(PERSON);
+
         return (
-            <h1>About me</h1>
+            <div>
+                <h1>{person.get('name')}</h1>
+
+                <p>{person.get('place')}</p>
+
+                <h2>Contacts</h2>
+
+                <ul>
+                    {person.get('contacts').entrySeq().map(([
+                        id,
+                        contact
+                    ]) => {
+                        return <li key={id}>{contact}</li>;
+                    })}
+                </ul>
+            </div>
         );
     }
 }
