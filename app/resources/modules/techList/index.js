e290dc3e3f82910906aeb18d6b6141cf65031694
Alexander Antonov <alexandr-post@yandex.ru>
2016-10-10 22:37:05 +0300

    module techList was created

---
+++ app/resources/modules/techList/index.js
@@ -0,0 +1,38 @@
+import Immutable from 'immutable';
+
+/**
+ * Get a list of tech used in the project.
+ *
+ * @param  {Map} tech
+ * @param  {Map} project
+ * @return {Map}
+ */
+export function techListByProject(tech, project) {
+    return Immutable.OrderedMap(project.get('tech').map((t) => {
+        return [String(t), tech.get(String(t))];
+    }));
+};
+
+/**
+ * Sort a list of tech by usage in projects.
+ *
+ * @param  {Map} tech
+ * @param  {Map} projects
+ * @param  {String} direction
+ * @return {Map}
+ */
+export function techListSortedByUsage(tech, projects, direction) {
+    const techSorted = tech.sortBy((t, id) => {
+        return projects.count((project) => {
+            return project.get('tech').includes(Number(id));
+        });
+    });
+
+    switch (direction) {
+        case 'DESC':
+            return techSorted.reverse();
+        case 'ASC':
+        default:
+            return techSorted;
+    }
+};
