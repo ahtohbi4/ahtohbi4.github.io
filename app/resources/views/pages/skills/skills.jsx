b2939484f3e0d705177ff62a1d8651bf8dbc8eed
Alexander Antonov <alexandr-post@yandex.ru>
2016-10-12 23:59:15 +0300

    skills/ page was updated

--- app/resources/views/pages/skills/skills.jsx
+++ app/resources/views/pages/skills/skills.jsx
@@ -5,6 +5,8 @@ import Immutable from 'immutable';
 import PROJECTS from '../../../../../data/projects.yaml';
 import TECH from '../../../../../data/tech.yaml';
 
+import {techListSortedByUsage} from '../../../modules/techList';
+
 import TagsGroup from '../../blocks/tags-group/tags-group.jsx';
 import Tag from '../../blocks/tag/tag.jsx';
 
@@ -14,28 +16,17 @@ import Tag from '../../blocks/tag/tag.jsx';
  */
 export default class Skills extends Component {
     render() {
-        const projects = Immutable.Map(PROJECTS);
-
-        /**
-         * Sorting technologies by the count of using in projects.
-         */
-        const techsSorted = Immutable.Map(TECH).sortBy((tech, id) => {
-            return projects.count((project) => {
-                return Immutable.Set(project.tech).includes(Number(id));
-            });
-        }).reverse();
-
         return (
             <div>
                 <h1>My skills</h1>
 
                 <TagsGroup>
-                    {techsSorted.entrySeq().map(([
+                    {techListSortedByUsage(Immutable.fromJS(TECH), Immutable.fromJS(PROJECTS), 'DESC').entrySeq().map(([
                         id,
                         tech
                     ]) => {
                         return (
-                            <Tag key={id} tagName="li">{tech.name}</Tag>
+                            <Tag key={id} tagName="li">{tech.get('name')}</Tag>
                         );
                     })}
                 </TagsGroup>
